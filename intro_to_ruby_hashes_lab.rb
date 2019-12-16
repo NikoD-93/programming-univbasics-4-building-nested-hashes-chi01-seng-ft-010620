af1c268319a285bd21574182737d8153a580cefe
Kieron Niko Dixon <nikodixon@gmail.com>
2019-12-16 02:31:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
 	 :rent_in_dollars => 
 	   {
 	    :one_piece_owned => 25,
-	    :second => 2,
+	    :two_pieces_owned => 2,
 	    :third => 3,
 	    :fourth => 4
 	  },
